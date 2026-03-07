a716182db9314f220aea8842997fa515e7f0feb2
Thomas Backman <serenity@exscape.org>
2026-03-07 23:53:29 +0100

    Add Events.cs, hiding the rain in Slippery Bridge

---
+++ src/Events.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace SimplifiedAnimations;
+
+// Hides the rain used in the Slippery Bridge event
+[HarmonyPatch(typeof(NRainVfx), nameof(NRainVfx.Create))]
+public static class RemoveRainVfxPatch
+{
+    public static void Postfix(ref NRainVfx __result)
+    {
+        __result.SelfModulate = Colors.Transparent;
+    }
+}
