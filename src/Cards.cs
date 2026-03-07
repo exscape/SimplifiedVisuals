1476f43881fe19f640a6714044085d6a3b793d81
Thomas Backman <serenity@exscape.org>
2026-03-07 23:44:06 +0100

    Add patch for NBigSlashVfx (+ impact vfx)

---
+++ src/Cards.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace SimplifiedAnimations;
+
+[HarmonyPatch(typeof(NBigSlashVfx), nameof(NBigSlashVfx._Ready))]
+public static class NBigSlashVfx__Ready_Patch
+{
+    public static bool Prefix(NBigSlashVfx __instance)
+    {
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(NBigSlashImpactVfx), nameof(NBigSlashImpactVfx._Ready))]
+public static class NBigSlashImpactVfx__Ready_Patch
+{
+    public static bool Prefix(NBigSlashImpactVfx __instance)
+    {
+        return false;
+    }
+
+}
