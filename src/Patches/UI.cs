b0e44dadc8cc5bc68ba140d3cce54249de57f6c1
Thomas Backman <serenity@exscape.org>
2026-03-10 17:53:02 +0100

    Also remove sparkles when rare glow is disabled

--- src/Patches/UI.cs
+++ src/Patches/UI.cs
@@ -1,9 +1,11 @@
-﻿using HarmonyLib;
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.Nodes.Vfx.Cards;
 
 namespace SimplifiedAnimations.Patches;
 
-// Disables the goldish glow around rare cards
+// Disables the goldish glow around rare cards -- also see the patch below
 [HarmonyPatch(typeof(NCardRareGlow), nameof(NCardRareGlow.Create))]
 public static class NCardRareGlow_Create_Patch
 {
@@ -12,6 +14,25 @@ public static class NCardRareGlow_Create_Patch
         if (!ModSettings.DisableRareCardGlow) return true;
         __result = null;
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(NCard), nameof(NCard.ActivateRewardScreenGlow))]
+public static class NCard_ActivateRewardScreenGlow_Patch
+{
+    public static void Postfix(NCard __instance)
+    {
+        if (!ModSettings.DisableRareCardGlow) return;
+
+        try
+        {
+            var sparkles = Traverse.Create(__instance).Field("_sparkles").GetValue<GpuParticles2D>();
+            sparkles.Visible = false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("[SimplifiedAnimations] Failed to remove sparkles from rare card reward");
+        }
     }
 }
 
