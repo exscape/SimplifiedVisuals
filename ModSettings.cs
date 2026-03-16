7db91fe5e7b47dc39f1a4dad457eb61ac32541a5
Thomas Backman <serenity@exscape.org>
2026-03-16 17:31:19 +0100

    Add config sections
    This uses a not-yet-merged BaseLib feature I implemented earlier today.
    A version with it should be available for download by the time this mod
    is released.

--- ModSettings.cs
+++ ModSettings.cs
@@ -4,31 +4,29 @@ namespace SimplifiedVisuals;
 
 public class ModSettings : SimpleModConfig
 {
-    // CombatEffects
+    [ConfigSection("CombatEffects")]
     public static bool DisableBigSlashEffect { get; set; } = true;
     public static bool DisablePurpleDoomOverlay { get; set; } = true;
-
     public static bool DisableRadialBlurEffect { get; set; } = true;
     public static bool DisableScreamEffect { get; set; } = true;
     public static bool DisableSpookyScreamEffect { get; set; } = true;
 
-
     public static bool DisableRegentAttackEffect { get; set; } = false;
     public static bool DisableSovereignBladeMovement { get; set; } = true;
 
-    // Environments
+    [ConfigSection("Environments")]
     public static bool DisableInsatiableSandfalls { get; set; } = true;
     public static bool DisableOtherInsatiableSandEffects { get; set; } = true;
     public static bool DisableRainEffect { get; set; } = true;
 
-    // Gameplay
+    [ConfigSection("Gameplay")]
     public static bool QuickerDraw { get; set; } = true;
 
-    // Timeline
+    [ConfigSection("Timeline")]
     public static bool FreezeBackgroundStars { get; set; } = true;
     public static bool HideConfetti { get; set; } = true;
     public static bool DisableUnlockShockwaves { get; set; } = true;
 
-    // UI
+    [ConfigSection("UserInterface")]
     public static bool DisableRareCardGlow { get; set; } = true;
 }
