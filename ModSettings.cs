2cd5ab6c7cbb3379289f56fc5eb17a122bd0d224
Thomas Backman <serenity@exscape.org>
2026-03-21 22:59:12 +0100

    Update default settings
    I'm not sure if the most reasonable is all off, all on, or a mix.
    This is a mix: the ones I guess are most likely to annoy people are
    on by default.

--- ModSettings.cs
+++ ModSettings.cs
@@ -7,26 +7,26 @@ public class ModSettings : SimpleModConfig
 {
     [ConfigSection("CombatEffects")]
     public static bool DisableBigSlashEffect { get; set; } = true;
-    public static bool DisablePurpleDoomOverlay { get; set; } = true;
+    public static bool DisablePurpleDoomOverlay { get; set; } = false;
     public static bool DisableRadialBlurEffect { get; set; } = true;
     public static bool DisableScreamEffect { get; set; } = true;
     public static bool DisableSpookyScreamEffect { get; set; } = true;
 
     public static bool DisableRegentAttackEffect { get; set; } = false;
-    public static bool DisableSovereignBladeMovement { get; set; } = true;
+    public static bool DisableSovereignBladeMovement { get; set; } = false;
 
     [ConfigSection("Environments")]
     public static bool DisableInsatiableSandfalls { get; set; } = true;
-    public static bool DisableOtherInsatiableSandEffects { get; set; } = true;
-    public static bool DisableRainEffect { get; set; } = true;
+    public static bool DisableOtherInsatiableSandEffects { get; set; } = false;
+    public static bool DisableRainEffect { get; set; } = false;
 
     [ConfigSection("Gameplay")]
-    public static bool QuickerDraw { get; set; } = true;
+    public static bool QuickerDraw { get; set; } = false;
 
     [ConfigSection("Timeline")]
     public static bool FreezeBackgroundStars { get; set; } = true;
     public static bool HideConfetti { get; set; } = true;
-    public static bool DisableUnlockShockwaves { get; set; } = true;
+    public static bool DisableUnlockShockwaves { get; set; } = false;
 
     [ConfigSection("UserInterface")]
     public static bool DisableRareCardGlow { get; set; } = true;
