0e20b6471124d34486351f347b67311ae7d57fd1
Thomas Backman <serenity@exscape.org>
2026-06-11 17:47:28 +0200

    Add HideAndDisable

--- Utils.cs
+++ Utils.cs
@@ -16,4 +16,16 @@ public static class NodeExtensions
         if (instance == null) return;
         action(instance);
     }
+
+    public static void HideAndDisable(this Node instance, string pattern, bool remainVisible = false, bool printDebug = false)
+    {
+        foreach (var child in instance.FindChildren(pattern, recursive: true, owned: false))
+        {
+            if (child is not CanvasItem canvasItem) continue;
+            Main.Logger.Debug($"Disabling node matching {pattern}: {child.Name} (type {child.GetType().Name})");
+            canvasItem.ProcessMode = Node.ProcessModeEnum.Disabled;
+            if (!remainVisible)
+                canvasItem.Visible = false;
+        }
+    }
 }
